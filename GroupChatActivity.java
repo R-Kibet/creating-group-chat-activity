56335d2c849487719a6e172f7d778ca6967fc36c
root <root@localhost.localdomain>
2022-01-22 11:46:45 -0500

    Creating a group chat activity where multiplle users could communicate

---
+++ GroupChatActivity.java
@@ -0,0 +1,118 @@
+package com.example.clone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.clone.Adapter.ChatAdapter;
+import com.example.clone.Models.MessageModel;
+import com.example.clone.databinding.ActivityGroupChatBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    ActivityGroupChatBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGroupChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GroupChatActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        //get sender id
+        final String senderId = FirebaseAuth.getInstance().getUid();
+        binding.userName.setText("Group Chat");//set group name
+
+
+        final ChatAdapter adapter = new ChatAdapter(messageModels, this);
+        binding.chatRecyclerView.setAdapter(adapter);
+
+        //set layout manager object
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+
+        //showing the messages in the group chat
+        database.getReference().child("Group Chat")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messageModels.clear();
+
+                        //use for loop to fetch and show data
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                        {
+                           MessageModel model = dataSnapshot.getValue(MessageModel.class);
+                           messageModels.add(model);
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
+
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String message = binding.entMess.getText().toString();//getting message from text filled
+                final  MessageModel model= new MessageModel(senderId,message); //sender id gotten from firebase auth
+
+
+                //set time stamp
+                model.setTimestamp(new Date().getTime());
+
+
+                //sending message to the database
+                binding.entMess.setText("");
+                database.getReference().child("Group Chat")
+                        .push()
+                        .setValue(model)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(@NonNull Void unused) {
+
+                            }
+                        })
+
+
+            }
+        });
+    }
+}
